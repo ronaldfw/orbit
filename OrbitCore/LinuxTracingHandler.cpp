718b6122052ab21ccae516c0199e50ad9b60e5e4
Pierric Gimmig <pierric@google.com>
2020-04-14 14:09:44 -0700

    Track integer register arguments on uprobes.

--- OrbitCore/LinuxTracingHandler.cpp
+++ OrbitCore/LinuxTracingHandler.cpp
@@ -132,7 +132,13 @@ void LinuxTracingHandler::OnFunctionCall(
     timer.m_UserData[1+i] = registers[i];
   }
 
-  tracing_buffer_->RecordTimer(std::move(timer));
+  const std::vector<uint64_t> registers = function_call.GetRegisters();
+  PRINT_VAR(registers.size());
+  for(size_t i = 0; i < registers.size(); ++i) {
+    ERROR("reg[%lu] = %lu", i, registers[i]);
+  }
+
+  session_->RecordTimer(std::move(timer));
 }
 
 uint64_t LinuxTracingHandler::ProcessStringAndGetKey(
